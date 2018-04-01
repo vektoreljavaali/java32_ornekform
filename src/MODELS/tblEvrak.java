9f9992fad719ea91a5bc4a5ceecd248d43a2d2a2
temelt <tanertemel@outlook.com>
2018-04-01 18:08:21 +0300

    ornek

---
+++ src/MODELS/tblEvrak.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODELS;
+
+
+/**
+ *
+ * @author vektorel
+ */
+public class tblEvrak {
+    
+    private String evrakkayittarihi;
+    private String evrakkayitnumarasi;
+    private String gelisyeri;
+    private String tarih;
+    private String sayi;
+    private String konu;
+    private String ekler;
+    private String havale;
+
+    
+    
+    public String getEvrakkayittarihi() {
+        return evrakkayittarihi;
+    }
+
+    public void setEvrakkayittarihi(String evrakkayittarihi) {
+        this.evrakkayittarihi = evrakkayittarihi;
+    }
+
+    public String getEvrakkayitnumarasi() {
+        return evrakkayitnumarasi;
+    }
+
+    public void setEvrakkayitnumarasi(String evrakkayitnumarasi) {
+        this.evrakkayitnumarasi = evrakkayitnumarasi;
+    }
+
+    public String getGelisyeri() {
+        return gelisyeri;
+    }
+
+    public void setGelisyeri(String gelisyeri) {
+        this.gelisyeri = gelisyeri;
+    }
+
+    public String getTarih() {
+        return tarih;
+    }
+
+    public void setTarih(String tarih) {
+        this.tarih = tarih;
+    }
+
+    public String getSayi() {
+        return sayi;
+    }
+
+    public void setSayi(String sayi) {
+        this.sayi = sayi;
+    }
+
+    public String getKonu() {
+        return konu;
+    }
+
+    public void setKonu(String konu) {
+        this.konu = konu;
+    }
+
+    public String getEkler() {
+        return ekler;
+    }
+
+    public void setEkler(String ekler) {
+        this.ekler = ekler;
+    }
+
+    public String getHavale() {
+        return havale;
+    }
+
+    public void setHavale(String havale) {
+        this.havale = havale;
+    }
+    
+    
+    
+}
